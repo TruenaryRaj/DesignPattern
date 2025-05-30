a3b49b59ca5c77651bc9c7925e08870fcaba37ab
TruenaryRaj <raj.truenary@gmail.com>
2025-05-30 17:28:46 +0545

    feat(test):basic unit test on calculator

---
+++ UnitTest/Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Calculator
+{
+    public class Program
+    {
+        public static Func<int, int, int> Add = (x, y) => x + y;
+        
+        public static Func<int, int, int> Subtract = (x, y) => x - y;
+        
+        public static Func<int, int, int> Multiply = (x, y) => x * y;
+    
+        public static Func<int, int, int> Division = (x, y) => x / y;
+        static void Main()
+        {
+          /*  Console.WriteLine($"Addition: {Add(3, 0)}");
+            Console.WriteLine($"Subtraction: {Subtract(3, 0)}");
+            Console.WriteLine($"Multiplication: {Multiply(3, 0)}");
+            Console.WriteLine($"Division: {Division(3, 1)}");*/
+        }
+    }
+}
---
+++ UnitTest/CalculatorTest/CalculatorTest.cs
@@ -0,0 +1,32 @@
+﻿namespace CalculatorTest
+{
+    using Calculator;
+    [TestClass]
+    public sealed class CalculatorTest
+    {
+       [TestMethod]
+        public void TestAdd()
+        {
+            Assert.AreEqual(5, Program.Add(3, 2));
+        }
+
+        [TestMethod]
+        public void TestSubtract()
+        {
+            Assert.AreEqual(3, Program.Subtract(3, 0));
+        }
+
+        [TestMethod]
+        public void TestMultiply()
+        {
+            Assert.AreEqual(0, Program.Multiply(3, 0));
+        }
+
+        [TestMethod]
+        public void TestDivide()
+        {
+            Assert.AreEqual(0, Program.Division(0, 3));
+        }
+    }
+
+}
